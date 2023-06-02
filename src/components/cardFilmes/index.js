f01adb95f900190c26738abfe7a1aecef40e20c0
jotahz <joaoextrme15@gmail.com>
2023-06-02 17:32:21 -0300

    mudanças

---
+++ src/components/cardFilmes/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Image,Text,TouchableOpacity} from 'react-native'
+import styles from './style'
+
+export default function CardMovies({titulo,imagem,nota}){
+
+    return(
+
+        <TouchableOpacity style = {styles.containerJogos}>
+
+        <Image style = {styles.images} source ={require (`../../Img/${imagem}` ) }/>
+        <Text style = {styles.textNota}> {titulo}</Text>
+        <Text style = {styles.textNota}> 8.9</Text>
+
+
+
+        </TouchableOpacity>
+
+    )
+
+
+
+}
---
+++ src/data/filmes.js
@@ -0,0 +1,65 @@
+const DATA = 
+[
+{
+    id:'01',
+    nome:'Adão Negro',
+    nota:'4.5',
+    imagem:'adao.jpg'
+},
+{
+    id: '02',
+    nome: 'Avatar',
+    nota:'3,5',
+    imagem:'avatar.jpg',
+},
+{
+    id: '03',
+    nome: 'Desaparecida',
+    nota:'8.5',
+    imagem:'desaparecida.jpg'
+},
+{
+    id: '04',
+    nome:'Gato de Botas II',
+    nota:'9.8',
+    imagem:'gato.jpg'
+},
+{
+    id: '05',
+    nome:'Homem Formiga Quantumania',
+    nota:'9.8',
+    imagem:'homemformiga.jpg'
+},
+{
+    id: '06',
+    nome:'Os Incríveis',
+    nota:'9.8',
+    imagem:'incriveis.jpg'
+},
+{
+    id: '07',
+    nome:'Super Mario Bros',
+    nota:'9.8',
+    imagem:'mario.jpg'
+},
+{
+    id: '08',
+    nome:'M3GAN',
+    nota:'9.8',
+    imagem:'megan.jpg'
+},
+{
+    id: '09',
+    nome:'Pânico VI',
+    nota:'9.8',
+    imagem:'panico.jpg'
+},
+{
+    id: '10',
+    nome:'Wandinha',
+    nota:'9.8',
+    imagem:'wandinha.jpg'
+},
+]
+
+export default DATA;
